3542e2636c6a4f2fe1705309466df0ab50a36a41
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-07-23 01:23:03 +0300

    Render map with old functionality

--- app.elm
+++ app.elm
@@ -3,6 +3,8 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
 import Json.Decode as Decode
+import Maps
+import Maps.Map as Map
 
 
 
@@ -18,17 +20,24 @@ main =
 
 -- MODEL
 
+type alias Lol = Maps.Model ()
 
 type alias Model =
   { topic : String
   , gifUrl : String
   , cameraStations : List String
+  , map : Lol
   }
 
 
 init : String -> (Model, Cmd Msg)
 init topic =
-  ( Model topic "waiting.gif" []
+  ( { topic = topic
+  , gifUrl = "waiting.gif"
+  , cameraStations = []
+  , map = Maps.defaultModel
+      |> Maps.updateMap (Map.setHeight 600)
+      |> Maps.updateMap (Map.setWidth 1000)}
   , getRandomGif topic
   )
 
@@ -41,6 +50,9 @@ type Msg
   = MorePlease
   | NewGif (Result Http.Error (List CameraStation))
   | Change String
+  | Left 
+  | Right
+  | MapMsg (Maps.Msg ())
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -60,14 +72,33 @@ update msg model =
     Change newContent ->
       ({model | topic = newContent}, Cmd.none)
 
+    Left ->
+    let lol = Debug.log "heippa" "lol"
+    in
+      (model, Cmd.none)
+    Right -> 
+      (model, Cmd.none)
+
+    MapMsg mapMsg ->
+      Maps.update mapMsg model.map
+      |> Tuple.mapFirst (\map -> { model | map = map })
+      |> Tuple.mapSecond (Cmd.map MapMsg)
+
 
 -- VIEW
+
+viewButton : String -> Html ()
+viewButton name =
+  button [ onClick () ] [ text name ]
 
 
 view : Model -> Html Msg
 view model =
   div []
     [ input [ placeholder "Type topic here", onInput Change ] []
+    , Html.map MapMsg <| Maps.view model.map
+    , Html.map (\_ -> Left) (viewButton "Left")
+    , Html.map (\_ -> Right) (viewButton "Right")
     -- , span [] [ text (toString model)]
     , button [ onClick MorePlease ] [ text "More Please!" ]
     , br [] []
