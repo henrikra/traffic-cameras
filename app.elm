5695f3c9e3986cf84375dce8d4fceb911bd429f2
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-07-22 22:16:04 +0300

    Use cat gif example

--- app.elm
+++ app.elm
@@ -1,13 +1,17 @@
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode
 
 
 
 main =
-  Html.beginnerProgram
-    { model = model
+  Html.program
+    { init = init "cats"
     , view = view
     , update = update
+    , subscriptions = subscriptions
     }
 
 
@@ -15,12 +19,17 @@ main =
 -- MODEL
 
 
-type alias Model = Int
+type alias Model =
+  { topic : String
+  , gifUrl : String
+  }
 
 
-model : Model
-model =
-  0
+init : String -> (Model, Cmd Msg)
+init topic =
+  ( Model topic "waiting.gif"
+  , getRandomGif topic
+  )
 
 
 
@@ -28,18 +37,21 @@ model =
 
 
 type Msg
-  = Increment
-  | Decrement
+  = MorePlease
+  | NewGif (Result Http.Error String)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Increment ->
-      model + 1
+    MorePlease ->
+      (model, getRandomGif model.topic)
 
-    Decrement ->
-      model - 1
+    NewGif (Ok newUrl) ->
+      (Model model.topic newUrl, Cmd.none)
+
+    NewGif (Err _) ->
+      (model, Cmd.none)
 
 
 
@@ -49,7 +61,35 @@ update msg model =
 view : Model -> Html Msg
 view model =
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
+    [ h2 [] [text model.topic]
+    , button [ onClick MorePlease ] [ text "More Please!" ]
+    , br [] []
+    , img [src model.gifUrl] []
     ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- HTTP
+
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+  let
+    url =
+      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+  in
+    Http.send NewGif (Http.get url decodeGifUrl)
+
+
+decodeGifUrl : Decode.Decoder String
+decodeGifUrl =
+  Decode.at ["data", "image_url"] Decode.string
